faa26485e43bf695eaf85b01cc2766d1a9832cfa
davidcoats <david@getsafetycone.com>
2024-04-30 16:31:09 -0700

    Interim changes.

---
+++ source/R5T.T0246/Code/Attributes/ScriptsMarkerAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using R5T.T0143;
+
+
+namespace R5T.T0246
+{
+    /// <summary>
+    /// Attribute indicating an interface contains scripts (default implementation methods on an interface that have no inputs).
+    /// The marker attribute is useful for surveying script interfaces and building a catalogue of scripts.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Interface, AllowMultiple = false, Inherited = false)]
+    [MarkerAttributeMarker]
+    public class ScriptsMarkerAttribute : Attribute,
+        IMarkerAttributeMarker
+    {
+        private readonly bool zIsScripts;
+
+        /// <summary>
+        /// Allows specifying that an interface is *not* a scripts interface.
+        /// This is useful for marking interfaces that end up canonical script code file locations, but are not script interfaces.
+        /// </summary>
+        public bool IsScripts
+        {
+            get
+            {
+                return this.zIsScripts;
+            }
+        }
+
+
+        public ScriptsMarkerAttribute(
+            bool isScripts = true)
+        {
+            this.zIsScripts = isScripts;
+        }
+    }
+}
---
+++ source/R5T.T0246/Code/Interfaces/IScriptsMarker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.T0246
+{
+    /// <summary>
+    /// Marker interface for interfaces containing scripts.
+    /// Allows methods-for, and extension methods-on, all types that contain script functionality.
+    /// </summary>
+    public interface IScriptsMarker
+    {
+    }
+}
